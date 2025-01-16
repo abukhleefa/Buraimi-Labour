2ea3cc91f633d6e9dedc7921b7fd67f813164fef
abukhleefa <93971292+abukhleefa@users.noreply.github.com>
2025-01-16 12:46:00 +0400

    Update texts.js

--- texts.js
+++ texts.js
@@ -77,7 +77,7 @@ const JS_eLang =
 
 			B_A_8: '- للاطلاع على آلية تسجيل طلب لإصدار تصريح لمزاولة العمل الخاصة (الأفراد) في الموقع الالكتروني لوزارة العمل يمكنك تنزيل الدليل التالي <a href="https://staff.mol.gov.om/DSMVD/CMS/WebSiteEServices/private_work_permit_manual.pdf" target="_blank">(إضغط هنا)</a>',
 
-			B_A_9: '-لبدء خدمة تسجيل طلب إصدار تصريح لمزاولة العمل الخاصة (الأفراد) <a href="https://eservices.mol.gov.om/wppa/list" target="_blank">(إضغط هنا)</a>',
+			B_A_9: '-لبدء خدمة تسجيل طلب إصدار تصاريح لمزاولة العمل الخاصة (الأفراد) <a href="https://eservices.mol.gov.om/wppa/list" target="_blank">(إضغط هنا)</a>',
 
 	
 
@@ -99,7 +99,7 @@ const JS_eLang =
 
 			B_B_8: '- للاطلاع على آلية تسجيل طلب لإصدار تصريح لمزاولة العمل الخاصة (المنشآت) في الموقع الالكتروني لوزارة العمل يمكنك تنزيل الدليل التالي <a href="https://staff.mol.gov.om/DSMVD/CMS/WebSiteEServices/commercial-work-permit_manual.pdf" target="_blank">(إضغط هنا)</a>',
 
-			B_B_9: '-لبدء خدمة تسجيل طلب إصدار تصريح لمزاولة العمل الخاصة (المنشأت) <a href="https://eservices.mol.gov.om/Wppa/list"_blank">(إضغط هنا)</a>',
+			B_B_9: '-لبدء خدمة تسجيل طلب إصدار تصاريح لمزاولة العمل التجاري (المنشأت) <a href="https://eservices.mol.gov.om/Wppa/list"_blank">(إضغط هنا)</a>',
 
 
 
